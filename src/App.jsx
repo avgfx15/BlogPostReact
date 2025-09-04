c50ebb06938e3b5f262d86c52d19fa4d76aeb753
avgfx15 <avgfx15@gmail.com>
2025-09-04 08:02:58 +0530

    Style added to post

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import { getApiData } from './API/ApiServices';
+import CardComponent from './Component/CardComponent';
 
 const App = () => {
   const [apiData, setApiData] = useState([]);
@@ -20,7 +21,16 @@ const App = () => {
     getData();
   }, []);
 
-  return <div>App</div>;
+  return (
+    <div className='flex flex-col items-center flex-wrap gap-4 p-4 justify-center'>
+      <h1>Blog Post</h1>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full'>
+        {apiData?.map((item) => (
+          <CardComponent key={item.id} cardItem={item} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Component/CardComponent.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const CardComponent = ({ cardItem }) => {
+  const { userId, title, body } = cardItem;
+  return (
+    <div className='border-2 border-black p-5 flex flex-col justify-center'>
+      <h2 className=' mb-4'>
+        UserId :<span className='text-2xl font-bold'>{userId}</span>
+      </h2>
+      <h2 className='mb-4 font-bold'>
+        Title : <span className='text-xl font-normal'>{title}</span>
+      </h2>
+      <h2 className='mb-4 font-bold'>
+        Message : <span className='text-xl font-normal'>{body}</span>
+      </h2>
+      <div className='bottom-0 mt-auto'>
+        <button className='bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700'>
+          Edit
+        </button>
+        <button className='bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700 ml-2'>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CardComponent;
